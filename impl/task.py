df846d4ee7d8d47262e7480d390c6ac785fcf171
Vladimir Ein <vein-github@outlook.com>
2022-04-18 16:31:15 +0500

    Restructured project directory a bit. Added a module for task-related classes.

---
+++ impl/task.py
@@ -0,0 +1,32 @@
+import datetime
+from enum import Enum
+
+
+class TaskStatus(Enum):
+    ACTIVE = "active"
+    DONE = "done"
+    CANCELLED = "cancelled"
+
+class Epic:
+    name = ""
+
+class Task:
+
+    status: TaskStatus = TaskStatus.ACTIVE
+
+    # For convenience, we store the first line of the description separately,
+    # in `summary`, since that's what will be displayed in the task list. The 
+    # rest of the description goes into the `desc` field.
+
+    # TODO: do we need to annotate the type on simple types like string?
+    summary: str = ""
+    desc: str = ""
+    comments = []
+    # TODO: decide on data type here. Do we want to include time? timezone?
+    deadline: datetime.date = None
+    epic: Epic = None
+    labels = []
+
+    def __init__(self, *arg, **kw):
+        pass
+
